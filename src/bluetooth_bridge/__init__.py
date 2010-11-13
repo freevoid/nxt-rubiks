be90232874efa860559af017e5377d9e668ba8e3
Nikolay Zakharov <nikolay.desh@gmail.com>
2010-11-13 15:12:06 +0300

    code improvements for Commander

--- src/bluetooth_bridge/__init__.py
+++ src/bluetooth_bridge/__init__.py
@@ -7,22 +7,28 @@ OPCODES = {
     'exit': 666,
 }
 
+class CommanderMeta(type):
+
+    def __new__(cls, name, bases, attrs):
+        for method_name, opcode in OPCODES.iteritems():
+            attrs[method_name] = CommanderMeta._make_operation(opcode)
+        return super(CommanderMeta, cls).__new__(cls, name, bases, attrs)
+
+    @staticmethod
+    def _make_operation(opcode):
+        op_prefix = '%s:' % opcode
+        def performer(self, *args):
+            msg = op_prefix + ':'.join(str(a) for a in args) + ':'
+            self.brick.message_write(self.inbox_id, msg)
+        return performer
+
+
 class Commander(object):
+
+    __metaclass__ = CommanderMeta
+
     def __init__(self, brick, inbox_id=INBOX_ID):
         self.brick = brick
         self.inbox_id = INBOX_ID
         object.__init__(self)
-
-    def __getattribute__(self, attrname):
-        if attrname != 'operation' and attrname in OPCODES:
-            return self.operation(OPCODES[attrname])
-        else:
-            return super(Commander, self).__getattribute__(attrname)
-
-    def operation(self, opcode):
-        op_prefix = '%s:' % opcode
-        def performer(*args):
-            msg = op_prefix + ':'.join(str(a) for a in args) + ':'
-            self.brick.message_write(self.inbox_id, msg)
-        return performer
 
