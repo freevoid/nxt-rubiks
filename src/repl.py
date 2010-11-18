b57fb0f650be6d06f8cecec9420287ceb4fc0cbc
Nikolay Zakharov <nikolay.desh@gmail.com>
2010-11-18 18:31:33 +0300

    repl.py initialize Commander in c variable

--- src/repl.py
+++ src/repl.py
@@ -1,7 +1,15 @@
 import nxt
 
+from bluetooth_bridge import Commander
+
 BRICK_HOST = '00:16:53:0D:17:8B'
 
 if __name__=='__main__':
-    b = nxt.find_one_brick(host=BRICK_HOST)
+    import sys
+    if len(sys.argv) > 1:
+        b = nxt.find_one_brick()
+    else:
+        b = nxt.find_one_brick(host=BRICK_HOST)
+
+    c = Commander(b)
 
