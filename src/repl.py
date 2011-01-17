a2811dca479fb329bff3eb5a7e25857f9545b9fb
Nikolay Zakharov <nikolay.desh@gmail.com>
2011-01-17 13:10:53 +0300

    updated repl

--- src/repl.py
+++ src/repl.py
@@ -1,4 +1,5 @@
-import nxt
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 from robots.rubiks_solver import *
 
@@ -7,9 +8,8 @@ BRICK_HOST = '00:16:53:0D:17:8B'
 if __name__=='__main__':
     import sys
     if len(sys.argv) > 1:
-        b = nxt.find_one_brick()
+        r = RubiksRobot()
     else:
-        b = nxt.find_one_brick(host=BRICK_HOST)
+        r = RubiksRobot(BRICK_HOST)
 
-    r = RubiksRobot(b)
 
